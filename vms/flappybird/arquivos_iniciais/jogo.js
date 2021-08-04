e48d70fd7eb255be2d54f2c4f379f462ce353c74
TROLLGAMES <73804955+yTROLLGAMES@users.noreply.github.com>
2021-08-04 15:08:40 -0300

    Add files via upload

---
+++ vms/flappybird/arquivos_iniciais/jogo.js
@@ -0,0 +1,7 @@
+console.log('[Coffeehub] FlappyBird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
